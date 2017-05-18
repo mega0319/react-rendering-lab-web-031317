61563c586e35ac2afe5bb236b94c8cecc49d8af8
Naz Khan <nazmuskhan@yahoo.com>
2017-05-18 14:02:58 -0400

    Done.

--- components/Rating.js
+++ components/Rating.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 export default class Rating extends React.Component {
 
+
   constructor(props) {
     super(props);
     this.state = {
       increasing: false,
       decreasing: false
     };
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      increasing: nextProps.rating > this.props.rating,
+      decreasing: nextProps.rating < this.props.rating
+    })
   }
 
   render() {
